42deb7ada8cbda13bae6ab4fc5c18c6f02495637
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-08-11 17:47:04 -0300

    Fix attribute access_token in fetchApi vacancies

--- js/vacancies/services/vacanciesService.js
+++ js/vacancies/services/vacanciesService.js
@@ -6,7 +6,7 @@ export async function getToken() {
     `${apiConfig.baseURL}${apiConfig.endpoints.getToken}`,
     'POST',
     apiConfig.credentials
-  ).then(data => data.token);  // Ajuste conforme o campo correto da resposta da API.
+  ).then(data => data.access_token);  // Ajuste conforme o campo correto da resposta da API.
 }
 
 export async function getVacancies(company_name) {
