c82cbdaf47334ad9cc7611773bd9ba3cc3a2b23e
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-08-14 12:41:44 -0300

    Added improved error handling in fetchHelper

--- js/vacancies/helpers/fetchHelper.js
+++ js/vacancies/helpers/fetchHelper.js
@@ -19,9 +19,38 @@ export async function fetchAPI(
     body: body ? JSON.stringify(body) : null,
   });
 
+  let errorMessage = "Erro na API";
+
   if (!response.ok) {
-    const errorData = await response.json();
-    throw new Error(errorData.message || "Erro na API");
+    // Personalizar a mensagem de erro com base no código de status
+    switch (response.status) {
+      case 400:
+        errorMessage = "Requisição inválida.";
+        break;
+      case 404:
+        errorMessage = "Recurso não encontrado.";
+        break;
+      case 500:
+        errorMessage = "Erro interno do servidor.";
+        break;
+      // Adicione outros códigos de status conforme necessário
+    }
+
+    // Tente obter o corpo da resposta
+    try {
+      const errorData = await response.json();
+
+      // Verifique se há uma mensagem de erro específica
+      if (errorData && errorData.message) {
+        errorMessage += " " + errorData.message;
+      } else if (errorData && errorData.company_name) {
+        errorMessage += " " + errorData.company_name.join(' ');
+      }
+    } catch (e) {
+      // A resposta pode não ser JSON, então use a mensagem de erro padrão
+    }
+
+    throw new Error(errorMessage);
   }
 
   return await response.json();
