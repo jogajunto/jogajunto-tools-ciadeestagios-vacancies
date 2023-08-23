a531225cba449d68ea7af24d8b6f850ddec0bc56
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-08-23 13:20:32 -0300

    Returned mode on create config file

---
+++ js/vacancies/generateConfig.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+require('dotenv').config();
+
+const configString = `
+export const apiConfig = {
+    baseURL: "https://adm.ciadeestagios.com.br",
+    endpoints: {
+        getToken: "/api/v1/auths/get-access-token",
+        getVacancies: "/api/v1/vacancies/",
+    },
+    credentials: {
+        email: "email",
+        password: "password",
+        abilities: "vacancies",
+        token: "${process.env.JJ_VACANCIES_FETCH_TOKEN}"
+    },
+}
+`;
+
+const directoryPath = path.join(__dirname, 'config');
+if (!fs.existsSync(directoryPath)) {
+    fs.mkdirSync(directoryPath, { recursive: true });
+}
+
+fs.writeFileSync(path.join(directoryPath, 'apiConfig.js'), configString);
+console.log('apiConfig.js gerado com sucesso.');
