cbffec5c612131ed6a8a89d5a60d0d627f475a81
rafaell1995 <rafaeldearaujop95@gmail.com>
2025-07-22 17:52:40 -0300

    fixed name token var to ACCESS_TOKEN

--- js/vacancies/generateConfig.js
+++ js/vacancies/generateConfig.js
@@ -13,7 +13,7 @@ export const apiConfig = {
         email: "email",
         password: "password",
         abilities: "vacancies",
-        token: "${process.env.JJ_VACANCIES_FETCH_TOKEN}"
+        token: "${process.env.ACCESS_TOKEN}"
     },
 }
 `;
